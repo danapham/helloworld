a932e3a0e313bd0f044be50fe43bd58443a00231
Dana Pham <danapham12@gmail.com>
2021-01-12 21:58:09 -0600

    completed exercise

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose from the following greetings: Howdy Y'all!!, Wassup?!, Hai");
+            var input = Console.ReadLine();
+
+            if (input == "")
+                input = "Howdy Y'all!!";
+
+            Console.WriteLine($"You chose {input}.");
+            
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan" };
+            
+            foreach(var animal in animals)
+            {
+
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
